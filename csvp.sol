ae6bbd94b36314bde09e9936efcd3931123f447b
Nandit <nanditmehra123@gmail.com>
2021-05-12 20:47:30 +0530

    added new functions, corrected syntax error

--- csvp.sol
+++ csvp.sol
@@ -3,51 +3,82 @@ pragma solidity >=0.4.22 <0.6.0;
 /// @title Croud Source Verification Protocol
 contract Csvp {
 
-    struct Vote {
-        address voter;
+    struct Voter {
         uint reputation; //reputation bw 1-1000
-        uint rating; //rating bw 1-1000
+        address id;
     }
     
     // Tweet refers to any post on any social nw, will add more fields later
     struct Tweet {
-        uint tweetId;
+        uint id;
+        uint rating;
+        address addedBy;
+        uint votesNumber;
+        Voter[] tweetVoters;
     }
 
-    mapping(Tweet => Vote[]) public votes; 
-    mapping(address => uint) public reputation; //Reputation is always a positive number. Reputation equals 0 means unrated
-    mapping(Tweet => uint) public tweetRating; 
+    mapping(address => Voter) public voters;
+    mapping(uint => Tweet) public tweets;
 
-    constructor() public {}
+    address public owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    function giveRightToVote(address voter) public {
+        require(
+            msg.sender == owner,
+            "Only owner can give the right to vote"
+        );
+        // @todo add to approved voters list
+    }
+
+    function addTweet(address addedBy, uint tweetId) public  {
+        Voter[] memory votersList;
+        Tweet memory t = tweets[tweetId]; 
+        t.id = tweetId;
+        t.rating = 0;
+        t.addedBy = addedBy;
+        t.votesNumber = 0;
+        t.tweetVoters = votersList;
+    }
 
     //On 100th Vote, this function will calculate the rating of the tweet
     function rateTweet(uint rating, uint tweetId) public {
-        Tweet tweet = Tweet{tweetId: tweetId};
+        // Tweet tweet = Tweet{tweetId: tweetId};
 
-        require(votes[tweet].length < 100);
-
-        votes[tweet].push(
-            Vote{
-                voter: msg.sender,
-                reputation: reputation[msg.sender], 
-                rating: rating
-            });
+        require(tweets[tweetId].votesNumber < 100);
         
-        if(votes[tweet].length == 100) {
-            //Time to calculate final rating of the Tweet
-            uint finalRating = 0;
-            for(uint i = 0; i < 100; i++) {
-                finalRating += votes[tweet][i].rating * (reputation / 1000); // Find a way to normalize the ratings
-            }
-            finalRating /= 100;
+        // @todo put a check for if a voter has already voted for a particular tweet
+        
+        Voter memory v = voters[msg.sender];
+        // @todo also send reputaiton here
+        tweets[tweetId].tweetVoters.push(v);
 
-            tweetRating[tweet] = finalRating;
-        }
-        calculateNewReputation(tweet);
+        // if(votes[tweet].length == 100) {
+        //     //Time to calculate final rating of the Tweet
+        //     uint finalRating = 0;
+        //     for(uint i = 0; i < 100; i++) {
+        //         finalRating += votes[tweet][i].rating * (reputation / 1000); // Find a way to normalize the ratings
+        //     }
+        //     finalRating /= 100;
+
+        //     tweetRating[tweet] = finalRating;
+        // }
+        // calculateNewReputation(tweet);
     }
 
-    function calculateNewReputation(Tweet tweet) internal {
+    function calculateTweetRating (uint tweetId) public 
+        returns (uint rating_)
+    {
+        for (uint r = 0; r < tweets[tweetId].votesNumber; r++) {
+            rating_ += tweets[tweetId].tweetVoters[r].reputation;
+        }
+    }
+
+    function calculateNewReputation(uint tweetId) internal {
         // People who guessed should be rewarded with high reputation and others should be penalized 
-        //TODO: To implement https://codeforces.com/blog/entry/102 
+        // @todo: To implement https://codeforces.com/blog/entry/102 
     }
 }
