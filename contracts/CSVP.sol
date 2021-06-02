107ca0446557481d35eed4f61b7d9efe18b1a5b4
Aashish Sharma <aashish.sharma@citrix.com>
2021-06-02 12:37:51 +0530

    Implemented Elo in solidity.

--- contracts/CSVP.sol
+++ contracts/CSVP.sol
@@ -1,29 +1,44 @@
 pragma solidity >=0.4.22 <0.6.0;
+import "./Elo.sol";
+import "./FixidityLib.sol";
+import "./ExponentLib.sol";
 
 /// @title Croud Source Verification Protocol
 contract Csvp {
 
-    struct Voter {
-        uint reputation; //reputation bw 1-1000
+    using FixidityLib for FixidityLib.Fixidity;
+    using LogarithmLib for FixidityLib.Fixidity;
+    using ExponentLib for FixidityLib.Fixidity;
+    
+    struct Vote {
+        uint rate; // Voter has to give the tweet a rating from 1 to 100.
         address id;
+        uint voteTime;
     }
     
-    // Tweet refers to any post on any social nw, will add more fields later
+    ELO.Scores scores;
+    FixidityLib.Fixidity fixidity;
+    
+    function fixiCheck() public view returns (int256) {
+        return fixidity.power_any(int256(16 * fixidity.fixed_1), -2000000000000000);
+    }
+    
+    // Tweet refers to any post on any social nw
     struct Tweet {
-        uint id;
         uint rating;
         address addedBy;
-        uint votesNumber;
-        Voter[] tweetVoters;
+        bool underReview;
+        Vote[] votes;
     }
 
-    mapping(address => Voter) public voters;
+    mapping(address => bool) public voters;
     mapping(uint => Tweet) public tweets;
 
     address public owner;
 
     constructor() public {
         owner = msg.sender;
+        fixidity.init(24);
     }
 
     function giveRightToVote(address voter) public {
@@ -31,54 +46,81 @@ contract Csvp {
             msg.sender == owner,
             "Only owner can give the right to vote"
         );
-        // @todo add to approved voters list
+        require(voters[voter] == false, "Voter Already has voting right");
+        voters[voter] = true;
     }
 
-    function addTweet(address addedBy, uint tweetId) public  {
-        Voter[] memory votersList;
-        Tweet memory t = tweets[tweetId]; 
-        t.id = tweetId;
-        t.rating = 0;
-        t.addedBy = addedBy;
-        t.votesNumber = 0;
-        t.tweetVoters = votersList;
+    function addTweet(uint tweetId) public  {
+        require(tweets[tweetId].addedBy == address(0), "Tweet already exists");
+        tweets[tweetId].addedBy = msg.sender;
+        tweets[tweetId].underReview = true;
     }
 
-    //On 100th Vote, this function will calculate the rating of the tweet
-    function rateTweet(uint rating, uint tweetId) public {
-        // Tweet tweet = Tweet{tweetId: tweetId};
-
-        require(tweets[tweetId].votesNumber < 100);
-        
-        // @todo put a check for if a voter has already voted for a particular tweet
-        
-        Voter memory v = voters[msg.sender];
-        // @todo also send reputaiton here
-        tweets[tweetId].tweetVoters.push(v);
-
-        // if(votes[tweet].length == 100) {
-        //     //Time to calculate final rating of the Tweet
-        //     uint finalRating = 0;
-        //     for(uint i = 0; i < 100; i++) {
-        //         finalRating += votes[tweet][i].rating * (reputation / 1000); // Find a way to normalize the ratings
-        //     }
-        //     finalRating /= 100;
-
-        //     tweetRating[tweet] = finalRating;
-        // }
-        // calculateNewReputation(tweet);
+    function addVote(uint tweetId, uint rating) public {
+        require(voters[msg.sender] == true, "This address isn't authorized to vote");
+        require(tweets[tweetId].addedBy != address(0), "Tweet isn't added for voting");
+        require(tweets[tweetId].underReview == true, "Tweet closed for review");
+        tweets[tweetId].votes.push(Vote({
+            rate: rating,
+            id: msg.sender,
+            voteTime: block.timestamp
+        }));
     }
 
-    function calculateTweetRating (uint tweetId) public 
-        returns (uint rating_)
-    {
-        for (uint r = 0; r < tweets[tweetId].votesNumber; r++) {
-            rating_ += tweets[tweetId].tweetVoters[r].reputation;
+    function calculateTweetRating (uint tweetId) public {
+        require(tweets[tweetId].addedBy != address(0), "Tweet isn't added for voting");
+        require(tweets[tweetId].underReview == true, "Poll already ended");
+        tweets[tweetId].underReview = false;
+        uint totalWeight = 0;
+        uint totalRating = 0;
+        for(uint i = 0; i < tweets[tweetId].votes.length; i++) {
+            // (pseudo)Randomly select 50% of the votes
+            address voterAddress = tweets[tweetId].votes[i].id;
+            if(uint(voterAddress) * block.timestamp % 2 == 0) {
+                totalWeight += scores.rating[voterAddress];
+                totalRating += tweets[tweetId].votes[i].rate * scores.rating[voterAddress];
+            }
         }
+        tweets[tweetId].rating = totalRating / totalWeight;
+        calculateNewReputation(tweetId);
     }
 
     function calculateNewReputation(uint tweetId) internal {
         // People who guessed should be rewarded with high reputation and others should be penalized 
-        // @todo: To implement https://codeforces.com/blog/entry/102 
+        Vote[] memory votes = tweets[tweetId].votes;
+        qSortVotes(votes, 0, votes.length - 1, tweets[tweetId].rating);
+        address[] memory ranked_list = new address[](votes.length);
+        for(uint i = 0; i < votes.length; i++) {
+            ranked_list[i] = votes[i].id;
+        }
+        ELO.updateRatings(scores, ranked_list, fixidity);
     }
+    
+    function abs(int value) pure internal returns (uint){
+        if (value>=0) return uint(value);
+        else return uint(-1*value);
+    }
+    
+    function compareVotes(Vote memory a, Vote memory b, uint tweetRating) internal pure returns (bool){
+        if(abs(int(a.rate) - int(tweetRating)) != abs(int(b.rate) - int(tweetRating)))
+            return abs(int(a.rate) - int(tweetRating)) < abs(int(b.rate) - int(tweetRating));
+        return a.voteTime < b.voteTime;
+    }
+    
+    function qSortVotes(Vote[] memory votes, uint left, uint right, uint tweetRating) internal pure{
+        if(left >= right)
+            return;
+        (votes[left], votes[(left + right) / 2]) = (votes[(left + right) / 2], votes[left]);
+        uint last = left;
+        for(uint i = left + 1; i <= right; i++) {
+            if(compareVotes(votes[i], votes[left], tweetRating)){
+                last++;
+                (votes[i], votes[last]) = (votes[last], votes[i]);
+            }
+        }
+        (votes[left], votes[last]) = (votes[last], votes[left]);
+        qSortVotes(votes, left, last - 1, tweetRating);
+        qSortVotes(votes, last + 1, right, tweetRating);
+    }
+    
 }
--- contracts/ELO.sol
+++ contracts/ELO.sol
@@ -1,102 +1,163 @@
 /**
- * Simple ELO score rating system based on
- * https://en.wikipedia.org/wiki/Elo_rating_system#Mathematical_details
+ * Refer https://www.codechef.com/ratings for exactitude
  */
+pragma solidity ^0.4.17;
+import "./FixidityLib.sol";
+import "./LogarithmLib.sol";
+import "./ExponentLib.sol";
+
 library ELO {
-    struct Score {
-        uint score;
-    }
+
+    using FixidityLib for FixidityLib.Fixidity;
+    using LogarithmLib for FixidityLib.Fixidity;
+    using ExponentLib for FixidityLib.Fixidity;
+    
     struct Scores {
-        mapping(address => Score) scores;
+        mapping(address => uint) rating;
+        mapping(address => uint) volatility;
+        mapping(address => uint) experience;
+    }
+    
+    //Getters
+    function getRating(Scores storage self, address player) internal view returns (uint) {
+        if (self.rating[player] == 0) {
+            return 1500;
+        }
+        return self.rating[player];
     }
 
-    /**
-     * Records a game result for a game between two players.
-     * Winner can be 0 to record a draw
-     */
-    function recordResult(Scores storage self,
-                          address player1, address player2, address winner) {
-        // Get current scores
-        uint scoreA = getScore(self, player1);
-        uint scoreB = getScore(self, player2);
-
-        // Calculate result for player A
-        int resultA = 1; // 0 = lose, 1 = draw, 2 = win
-        if (winner == player1) {
-            resultA = 2;
+    function getVolatility(Scores storage self, address player) internal view returns (uint) {
+        if (self.rating[player] == 0) {
+            return 125;
         }
-        else if (winner == player2) {
-            resultA = 0;
-        }
-
-        // Calculate new score
-        var (changeA, changeB) = getScoreChange(int(scoreA)-int(scoreB), resultA);
-        setScore(self, player1, uint(int(scoreA) + changeA));
-        setScore(self, player2, uint(int(scoreB) + changeB));
+        return self.rating[player];
     }
 
-    /**
-     * Table based expectation formula
-     * E = 1 / ( 1 + 10**((difference)/400))
-     * Table calculated based on inverse: difference = (400*log(1/E-1))/(log(10))
-     * scoreChange = Round( K * (result - E) )
-     * K = 20
-     * Because curve is mirrored around 0, uses only one table for positive side
-     * Returns (scoreChangeA, scoreChangeB)
-     */
-    function getScoreChange(int difference, int resultA) returns (int, int) {
-        bool reverse = (difference > 0); // note if difference was positive
-        uint diff = abs(difference); // take absolute to lookup in positive table
-        // Score change lookup table
-        int scoreChange = 10;
-        if (diff > 636) scoreChange = 20;
-        else if (diff > 436) scoreChange = 19;
-        else if (diff > 338) scoreChange = 18;
-        else if (diff > 269) scoreChange = 17;
-        else if (diff > 214) scoreChange = 16;
-        else if (diff > 168) scoreChange = 15;
-        else if (diff > 126) scoreChange = 14;
-        else if (diff > 88) scoreChange = 13;
-        else if (diff > 52) scoreChange = 12;
-        else if (diff > 17) scoreChange = 11;
-        // Depending on result (win/draw/lose), calculate score changes
-        if (resultA == 2) {
-            return ((reverse ? 20-scoreChange : scoreChange ),
-                    (reverse ? -scoreChange : -(20-scoreChange)));
-        }
-        else if (resultA == 1) {
-            return ((reverse ? 10-scoreChange : scoreChange-10 ),
-                    (reverse ? -(10-scoreChange) : -(scoreChange-10)));
-        }
-        else {
-            return ((reverse ? scoreChange-20 : -scoreChange ),
-                    (reverse ? scoreChange : -(scoreChange-20)));
-        }
+    function getExperience(Scores storage self, address player) internal view returns (uint) {
+        return self.experience[player];
     }
 
-    function abs(int value) returns (uint){
-        if (value>=0) return uint(value);
-        else return uint(-1*value);
+    //Setters
+    function setRating(Scores storage self, address player, uint rating) internal {
+        self.rating[player] = rating;
     }
 
-    /**
-     * Get current score for player
-     */
-    function getScore(Scores storage self, address player) constant returns (uint) {
-        if (self.scores[player].score <= 100) {
-            return 100;
+    function setVolatility(Scores storage self, address player, uint volatility) internal {
+        if (volatility < 75) {
+            self.volatility[player] = 75;
         }
-        return self.scores[player].score;
+        else if (volatility > 200) {
+            self.volatility[player] = 200;
+        }
+        else 
+            self.volatility[player] = volatility;
     }
 
-    /**
-     * Set score for player
-     */
-    function setScore(Scores storage self, address player, uint score) {
-        if (score <= 100) {
-            self.scores[player].score = 100;
-        } else {
-            self.scores[player].score = score;
+    function incrementExperience(Scores storage self, address player) internal {
+        self.experience[player]++;
+    }
+
+//Returns a fixed valuee
+    function elo_ab(FixidityLib.Fixidity storage fixidity, uint rating_a, uint rating_b, uint volatility_a, uint volatility_b) view internal returns (int256) {
+        int256 vol_sqrt = int256(volatility_a * volatility_a + volatility_b * volatility_b);
+        vol_sqrt *= fixidity.fixed_1; //converting vol_sq to a fixed number
+        vol_sqrt = fixidity.root_n(vol_sqrt, 2);
+        
+        int256 t = fixidity.subtract(int256(rating_a) * fixidity.fixed_1, int256(rating_b) * fixidity.fixed_1);
+        t = fixidity.divide(t, vol_sqrt);
+        t = fixidity.power_any(4 * fixidity.fixed_1, t);
+        
+        return fixidity.reciprocal(fixidity.add(fixidity.fixed_1, t));
+    }
+
+//Returns a fixed value
+    function findCompetitionFactor(Scores storage self, address[] memory ranked_list, FixidityLib.Fixidity storage fixidity) view internal returns (int256) {
+        int256 n = int256(ranked_list.length) * fixidity.fixed_1; //n is fixed_1
+        
+        int256 mean_rating = 0;
+        for(uint i = 0; i < ranked_list.length; i++) {
+            mean_rating += int256(getRating(self, ranked_list[i]));
+        }
+        mean_rating *= fixidity.fixed_1; //converting mean_rating to a fixed number
+        mean_rating = fixidity.divide(mean_rating, n);
+        
+        int256 mean_squared_volatility = 0;
+        for(i = 0; i < ranked_list.length; i++) {
+            mean_squared_volatility += int256(getVolatility(self, ranked_list[i]) * getVolatility(self, ranked_list[i]));
+        }
+        mean_squared_volatility *= fixidity.fixed_1; //converting mean_squared_volatility to a fixed number
+        mean_squared_volatility = fixidity.divide(mean_squared_volatility, n);
+        
+        int256 variance = 0;
+        for(i = 0; i < ranked_list.length; i++) {
+            int256 mean_diff = fixidity.subtract(int256(getRating(self, ranked_list[i])) * fixidity.fixed_1, mean_rating);
+            mean_diff = fixidity.multiply(mean_diff, mean_diff);
+            variance = fixidity.add(variance, mean_diff);
+        }
+        variance = fixidity.divide(variance, fixidity.subtract(n, fixidity.fixed_1));
+        
+        return fixidity.root_n(fixidity.add(mean_squared_volatility, variance), 2);
+    }
+    
+    function updateRatings(Scores storage self, address[] memory ranked_list, FixidityLib.Fixidity storage fixidity) public {
+        int256 n = int256(ranked_list.length) * fixidity.fixed_1; //n is fixed_1
+        int256 cf = findCompetitionFactor(self, ranked_list, fixidity);
+        
+        for(uint i = 0; i < ranked_list.length; i++) {
+            int256 actual_rank = int256(i + 1) * fixidity.fixed_1;
+            int256 expected_rank = 0;
+            
+            //For loop for calculating expected ranked_list
+            for(uint j = 0; j < ranked_list.length; j++) 
+                if(i != j) {
+                    expected_rank = fixidity.add(expected_rank, 
+                                    elo_ab(fixidity, getRating(self, ranked_list[i]), getRating(self, ranked_list[j]), getVolatility(self, ranked_list[i]), getVolatility(self, ranked_list[j])));
+                }
+                
+            int256 actual_performance = fixidity.log_any(fixidity.divide(n, actual_rank), 2 * fixidity.fixed_1);
+            int256 expected_performance = fixidity.log_any(fixidity.divide(n, fixidity.subtract(expected_rank, fixidity.fixed_1)), 2 * fixidity.fixed_1);
+            
+            int256 tp = int256(getExperience(self, ranked_list[i])) * fixidity.fixed_1;
+            int256 rating_weight = fixidity.divide(fixidity.add(fixidity.multiply(tp, 4 * fixidity.fixed_1 / 10),  2 * fixidity.fixed_1 / 10),
+                                    fixidity.add(fixidity.multiply(tp, 7 * fixidity.fixed_1 / 10),  6 * fixidity.fixed_1 / 10));
+            int256 vol_weight = fixidity.divide(fixidity.add(fixidity.multiply(tp, 5 * fixidity.fixed_1 / 10),  8 * fixidity.fixed_1 / 10),
+                                    fixidity.add(tp, 6 * fixidity.fixed_1 / 10));
+                                    
+            int256 maxRating = 100 + 75 / (tp + 1) + 50000 / (abs((int256(getRating(self, ranked_list[i]))) - 1500) + 500); //NOT FIXED
+            
+            int256 oldRating = int256(getRating(self, ranked_list[i])); //NOT FIXED
+            
+            //Update Ratings
+            int256 ratingChange = fixidity.multiply(fixidity.multiply(fixidity.subtract(actual_performance, expected_performance), cf), rating_weight);
+            ratingChange /= fixidity.fixed_1; //Unfixing
+            int256 newRating = oldRating + ratingChange;
+            if(newRating < maxRating)
+                setRating(self, ranked_list[i], uint(newRating));
+            else
+                setRating(self, ranked_list[i], uint(maxRating));
+                
+            //Update volatility
+            int256 volatilitySq = int256(getVolatility(self, ranked_list[i]));
+            volatilitySq *= volatilitySq;
+            volatilitySq *= fixidity.fixed_1; //Fixing
+            
+            int256 ratingChangeSq = int256(getRating(self, ranked_list[i])) - oldRating;
+            ratingChangeSq * ratingChangeSq;
+            ratingChangeSq *= fixidity.fixed_1; //Fixing
+            
+            int256 newVolatility = fixidity.add(fixidity.multiply(vol_weight, ratingChangeSq), volatilitySq);
+            newVolatility = fixidity.divide(newVolatility, fixidity.add(vol_weight, 11 * fixidity.fixed_1 / 10));
+            newVolatility = fixidity.root_n(newVolatility, 2);
+            newVolatility /= fixidity.fixed_1; //Unfixing
+            setVolatility(self, ranked_list[i], uint(newVolatility));
+            
+            incrementExperience(self, ranked_list[i]);
         }
     }
+    
+    function abs(int value) pure internal returns (int){
+        if (value>=0) return value;
+        else return -1*value;
+    }
+    
 }
